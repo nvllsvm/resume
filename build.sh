bbf7431bae6cbfecda5c0cfab708bfbe25e611af
Andrew Rabert <ar@nullsum.net>
2021-04-01 19:56:54 -0400

    Because rootless podman breaks every-fucking-release

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")" || exit 1
 IMAGE='nvllsvm_resume'
 
-if ! podman inspect "$IMAGE" > /dev/null 2>&1; then
-    podman build . --pull -t "$IMAGE"
+if ! docker inspect "$IMAGE" > /dev/null 2>&1; then
+    docker build . --pull -t "$IMAGE"
 fi
-podman run -w "$(pwd)" -v "$(pwd):$(pwd)" "$IMAGE" pdflatex resume.tex
+docker run -w "$(pwd)" -v "$(pwd):$(pwd)" "$IMAGE" pdflatex resume.tex
