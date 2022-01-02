f7abbb59d415ab9edc80b94759e0fe5ce8043a21
Andrew Rabert <ar@nullsum.net>
2022-01-02 15:45:19 -0500

    Prefer podman

--- build.sh
+++ build.sh
@@ -3,6 +3,14 @@ set -e
 cd "$(dirname "$0")" || exit 1
 IMAGE='nvllsvm_resume'
 
+if command -v podman > /dev/null; then
+    docker() {
+        podman "$@"
+        ret=$?
+        return $ret
+    }
+fi
+
 if ! docker inspect "$IMAGE" > /dev/null 2>&1; then
     docker build . --pull -t "$IMAGE"
 fi
